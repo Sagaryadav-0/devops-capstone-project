2a29931c8275b5f4945381e232fc6a3926311494
sagar yadav <sagarrao0122@gmail.com>
2026-09-24 22:12:38 +0530

    Add Talisman security headers

--- service/__init__.py
+++ service/__init__.py
@@ -4,6 +4,7 @@ Package for the application models and service routes
 This module creates and configures the Flask app and sets up the logging
 and SQL database
 """
+
 import sys
 from flask import Flask
 from flask_talisman import Talisman
@@ -19,16 +20,18 @@ Talisman(app)
 
 # Import the routes after the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
-from service import routes, models  # noqa: F401 E402
+from service import routes, models  # noqa: F401, E402
 
 # pylint: disable=wrong-import-position
-from service.common import error_handlers, cli_commands  # noqa: F401 E402
+from service.common import error_handlers, cli_commands  # noqa: F401, E402
 
 # Set up logging for production
 log_handlers.init_logging(app, "gunicorn.error")
 
 app.logger.info(70 * "*")
-app.logger.info("  A C C O U N T   S E R V I C E   R U N N I N G  ".center(70, "*"))
+app.logger.info(
+    "  A C C O U N T   S E R V I C E   R U N N I N G  ".center(70, "*")
+)
 app.logger.info(70 * "*")
 
 try:
