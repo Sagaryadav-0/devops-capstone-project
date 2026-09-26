4fb61bdc30038a224d1ce6ab7d7fd93466130ecb
sagar yadav <sagarrao0122@gmail.com>
2026-09-26 18:56:36 +0530

    HTTPS redirect for tests

--- service/__init__.py
+++ service/__init__.py
@@ -16,7 +16,10 @@ app = Flask(__name__)
 app.config.from_object(config)
 
 # Configure security headers
-Talisman(app)
+Talisman(
+    app,
+    force_https=False,
+)
 
 # Import the routes after the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
