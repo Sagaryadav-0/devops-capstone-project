586303e9f70cef796aba01f9321006a254b919ab
sagar yadav <sagarrao0122@gmail.com>
2026-09-26 18:35:33 +0530

    Use DATABASE_URI environment variable

--- service/config.py
+++ service/config.py
@@ -3,17 +3,11 @@ Global Configuration for Application
 """
 import os
 
-
-# Get configuration from environment
-DATABASE_URI = os.getenv("DATABASE_URI")
-
-# Build DATABASE_URI from environment if not found
-if not DATABASE_URI:
-    DATABASE_USER = os.getenv("DATABASE_USER", "postgres")
-    DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "postgres")
-    DATABASE_NAME = os.getenv("DATABASE_NAME", "postgres")
-    DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
-    DATABASE_URI = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:5432/{DATABASE_NAME}"
+# Database Configuration
+DATABASE_URI = os.getenv(
+    "DATABASE_URI",
+    "postgresql://postgres:postgres@localhost:5432/postgres",
+)
 
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
